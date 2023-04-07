e936d64c33b859bb695e7e14a3d0b82230374087
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-07 07:35:19 -0600

    Add cell methods

--- lib/ship.rb
+++ lib/ship.rb
@@ -6,7 +6,7 @@ class Ship
   def initialize(name, length)
     @name = name
     @length = length
-    @health = health
+    @health = length
   end
 
   def sunk? 
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -1,6 +1,6 @@
 require './lib/ship'
 
-Rspec.describe Ship do
+RSpec.describe Ship do
   it 'exists' do
     cruiser = Ship.new("Cruiser", 3)
     expect(cruiser).to be_an_instance_of(Ship)
