a4f10a93b7a30cfad89d66e5d60572c07170ccdc
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-10 07:09:59 -0600

    Add coordinate validation tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -32,4 +32,28 @@ RSpec.describe Board do
       expect(@board.valid_coordinate?("A22")).to be  false
     end
   end
+
+  describe 'placment validation' do
+    it 'checks before ship placement' do
+      expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
+      expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])). to be false
+    end
+  
+    it 'has consecutive coordinates' do
+      expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
+      expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
+      expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
+      expect(board.valid_placement?(submarine, ["C1", "B1"])).to be false
+    end
+  
+    it 'is not diagonal' do
+      expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
+      expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
+    end
+
+    it '#valid_placement' do
+      expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
+      expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
+    end
+  end
 end
