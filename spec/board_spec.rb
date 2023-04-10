0a7703c9e65b2df64745ffdfdeb7ea4c564ff69c
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-10 11:55:42 -0600

    Instantiate ships in spec tests per PR comments

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -35,11 +35,15 @@ RSpec.describe Board do
 
   describe 'placment validation' do
     it 'checks before ship placement' do
+      cruiser = Ship.new("Cruiser", 3)  
+      submarine = Ship.new("Submarine", 2) 
       expect(board.valid_placement?(cruiser, ["A1", "A2"])).to be false
       expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])). to be false
     end
   
     it 'has consecutive coordinates' do
+      cruiser = Ship.new("Cruiser", 3)  
+      submarine = Ship.new("Submarine", 2) 
       expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to be false
       expect(board.valid_placement?(submarine, ["A1", "C1"])).to be false
       expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to be false
@@ -47,11 +51,15 @@ RSpec.describe Board do
     end
   
     it 'is not diagonal' do
+      cruiser = Ship.new("Cruiser", 3)  
+      submarine = Ship.new("Submarine", 2) 
       expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to be false
       expect(board.valid_placement?(submarine, ["C2", "D3"])).to be false
     end
 
     it '#valid_placement' do
+      cruiser = Ship.new("Cruiser", 3)  
+      submarine = Ship.new("Submarine", 2) 
       expect(board.valid_placement?(submarine, ["A1", "A2"])).to be true
       expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to be true
     end
